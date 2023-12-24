4efd4e096462b89a1e0bf2daa43add6653afc0a7
AyushTamang <ayushtamang1995@gmail.com>
2023-12-24 10:02:25 +0545

    day 6

---
+++ Sunway/day6.py
@@ -0,0 +1,52 @@
+# i = 0
+# while i < 5:
+#     print("Hello World!")
+#     i = i + 1
+
+# names = ['ram', 'shyam', 'hari', 'rita']
+# i = 0 
+# while i < len(names):
+#     print(names[i])
+#     i = i + 1
+
+# num = int(input("Enter a number: "))
+# if num % 2 == 0:
+#     print(f"{num} is even.")
+# else:
+#     print(f"{num} is odd.")
+
+# while True:
+#     num = int(input("Enter a number: "))
+#     if num % 2 == 0:
+#         print(f"{num} is even.")
+#     else:
+#         print(f"{num} is odd.")
+
+# names = ['ram', 'shyam', 'rita', 'sita']
+# for item in names:
+#     print(item)
+
+# numbers = [1,2,3,4,5,6,7,8,9,10]
+# for num in numbers:
+#     if num % 2 == 0:
+#         print(num)
+
+# names = ['ram', 'rythm', 'riz', 'rita']
+# for name in names:
+#     if 'a' in name.lower() or 'e' in name.lower() or 'i' in name.lower() or 'o' in name.lower() or 'u' in name.lower():
+#         print(f"There is vowel letter in {name}.")
+#     else:
+#         print(f"There is no vowel letter in {name}.")
+
+# names = ['ram', 'shyam', 'hari']
+# for name in names:
+#     print(name)
+#     break
+# print("End!")
+
+# names = ['ram', 'shyam', 'hari']
+# for name in names:
+#     if name.lower() == "shyam":
+#         print("Name found.")
+#         break
+
