bda5ed92d8fbfe9cfe11b9496ef93ff233900e43
AyushTamang <ayushtamang1995@gmail.com>
2023-12-28 07:27:52 +0545

    add comment and modify

--- Sunway/day7.py
+++ Sunway/day7.py
@@ -3,11 +3,15 @@ def find_max(l):
 
 print(find_max([1,2,3,4,5]))
 
+#Write a function that takes list as an arguement and rounds up
+
 def find_avg(l):
-    sum = 0
-    for item in l:
-        sum = sum + item
-    avg = sum / len(l)
-    return avg
+    # sum = 0
+    # for item in l:
+    #     sum = sum + item
+    # avg = sum / len(l)
+
+    return round(sum(l) / len(l), 2)
+    # return round(avg, 2)
 
 print(find_avg([1,2,3,4,5]))
